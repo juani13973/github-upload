e6e9a79a8179ae4d579aa254531f88e83972acb6
juani13973 <77895272+juani13973@users.noreply.github.com>
2021-01-26 21:36:47 +0100

    Initial commit moving project

---
+++ hello.cpp
@@ -0,0 +1,12 @@
+/* hello.cpp */
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout << "Please enter your last name: ";
+string name;
+    cin >> name;
+    cout << "Hello, " << name;
+    cout << "\n";
+return 0;
+}
